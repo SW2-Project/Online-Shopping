9697de1fe7b0b98f6f050dfe8e69afde2fbee061
Mostafa213 <sasa7amo2232@gmail.com>
2020-03-27 16:36:48 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/Admin', function(){
     return view('Admin.Admin');
-});
+})->name('Admin');
 
 Route::get('/Add_product', function(){
     return view('Add_product.Add_product');
@@ -41,12 +41,12 @@ Route::post('deleteproduct', 'deleteproduct@delete');
 
 Route::get('/Register', function(){
     return view('Register.Register');
-});
+})->name('Register');
 Route::get('/Login', function(){
     return view('Login.Login');
-});
+})->name('Login');
 
 Route::get('/delete_product', function(){
     return view('delete_product.delete_product');
-});
+})->name('delete_product');
 
